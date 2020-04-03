219b2224dfbad0da0a8f6fd90820a2f2cd387ee4
mostafaElkenany <mostafa.elkenany@gmail.com>
2020-04-03 17:34:02 +0200

    -removed navbar.css (unnecessary)

--- app/controllers/groups_users_controller.rb
+++ app/controllers/groups_users_controller.rb
@@ -1,8 +1,8 @@
 class GroupsUsersController < ApplicationController
   def create
     if (params[:name].present?)
-      # @user = User.find_by(name: params[:name])
-      # p @user
+      @user = User.find_by(name: params[:name])
+      p @user
     end
     # p params
     redirect_back(fallback_location: root_path)
