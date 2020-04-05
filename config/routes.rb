41ecc8923ec786d5f3cc480277102d5a7a12a18b
Islam <iabdelhamid3@gmail.com>
2020-04-05 23:24:22 +0200

    before merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
-resources :orders
-  root 'welcome#index'
+  resources :orders
+    root 'welcome#index'
+    
+    post 'groups_users' , to: 'groups_users#create'
+    delete 'groups_users/destroy/:id' , to: 'groups_users#destroy', as: :remove_user
   
-  post 'groups_users' , to: 'groups_users#create'
-  delete 'groups_users/destroy/:id' , to: 'groups_users#destroy', as: :remove_user
-
-  resources :groups
-  get  'users/index',to:"users#index"
-  get 'search/groups', to: "orders#searchGroups"
-  get 'search/friends', to: "orders#searchFriends"
-  devise_for :users, controllers: 
-        { sessions: 'users/sessions' ,
-          registrations: 'users/registrations',
-          passwords: 'users/passwords',
-          confirmations: 'users/confirmations',
-          omniauth: 'users/omniauth',
-          unlocks: 'users/unlocks'
-        }
-end
+    resources :groups
+    get  'users/index',to:"users#index"
+    get 'search/groups', to: "orders#searchGroups"
+    get 'search/friends', to: "orders#searchFriends"
+    devise_for :users, controllers: 
+          { sessions: 'users/sessions' ,
+            registrations: 'users/registrations',
+            passwords: 'users/passwords',
+            confirmations: 'users/confirmations',
+            omniauth: 'users/omniauth',
+            unlocks: 'users/unlocks'
+          }
+  end
+  
