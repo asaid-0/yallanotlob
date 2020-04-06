59be085cbc20e667b54731ad65950eaedbaf268e
omarMohamedAbdo <omarknighto@gmail.com>
2020-04-06 04:58:06 +0200

    Adding Validation For Friend Email

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -4,12 +4,20 @@ class FriendsController < ApplicationController
     end
   
     def create
-      if (params[:email].present?)
-        @new_friendship= Friendship.new;
-        @new_friendship.user_id = current_user.id
-        @new_friendship.friend_id = User.find_by(email: params[:email]).id
-        @new_friendship.save 
-      end
+
+        if (params[:email].present?)
+          @friend = User.find_by(email: params[:email])
+          if (@friend.present?)
+            @new_friendship= Friendship.new;
+            @new_friendship.user_id = current_user.id
+            @new_friendship.friend_id = User.find_by(email: params[:email]).id
+            @new_friendship.save 
+          else
+            flash[:friend_error] = "Friend Email doesnt exist"
+          end
+        else
+          flash[:friend_error] = "You didnt type any Email Address"
+        end
       redirect_back(fallback_location: root_path)
     end
   
