7eb7f01baf96299a7bb66f4eb075374a567ad06e
omarMohamedAbdo <omarknighto@gmail.com>
2020-04-06 05:23:35 +0200

    Adding More Validations to Friends

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -4,14 +4,18 @@ class FriendsController < ApplicationController
     end
   
     def create
-
         if (params[:email].present?)
           @friend = User.find_by(email: params[:email])
           if (@friend.present?)
-            @new_friendship= Friendship.new;
-            @new_friendship.user_id = current_user.id
-            @new_friendship.friend_id = User.find_by(email: params[:email]).id
-            @new_friendship.save 
+            @friendship= Friendship.find_by(friend_id:  @friend.id, user_id: current_user.id);
+            if (!@friendship.present?)
+              @new_friendship= Friendship.new;
+              @new_friendship.user_id = current_user.id
+              @new_friendship.friend_id = User.find_by(email: params[:email]).id
+              @new_friendship.save
+            else
+              flash[:friend_error] = "You already Have this friend in your list"
+            end
           else
             flash[:friend_error] = "Friend Email doesnt exist"
           end
