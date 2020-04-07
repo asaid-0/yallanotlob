166c9c5a8309f6fc3095faa6a559db7e1531f1d7
mostafaElkenany <mostafa.elkenany@gmail.com>
2020-04-07 20:09:21 +0200

    -added name to sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,13 @@ class ApplicationController < ActionController::Base
         redirect_to :new_user_session
       end
     end
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
