abff2f6fce6043a6d328221daeff42c0101c6dc5
mostafaElkenany <mostafa.elkenany@gmail.com>
2020-04-10 20:18:44 +0200

    -added link for user profile in notifications

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     require 'json'
     
     def index
-        @user = Order.find_by(id: params[:id]).user if Order.find_by(id: params[:id]) 
+        @user = User.find_by(id: params[:id]) if User.find_by(id: params[:id]) 
         # render :index
     end
     
